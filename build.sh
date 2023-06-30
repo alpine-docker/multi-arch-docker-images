fe8db317a23716e6cb775c2e9181ddb082add355
Bill Wang <bill.wang>
2023-06-30 14:14:24 +1000

    add first application, links

---
+++ build.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# Prerequisite
+# Make sure you set secret enviroment variables in CI
+# DOCKER_USERNAME
+# DOCKER_PASSWORD
+
+# usage
+set -e
+
+Usage() {
+  echo "$0 <image_name>"
+}
+
+pushd $1
+image="alpine/$1"
+platform="linux/arm/v7,linux/arm64/v8,linux/arm/v6,linux/amd64,linux/ppc64le,linux/s390x" 
+
+curl -sL "https://raw.githubusercontent.com/alpine-docker/multi-arch-libs/stable/functions.sh" -o functions.sh
+source functions.sh
+
+tag="latest"
+build_arg="VERSION=${tag}"
+
+echo "build image for ${tag}"
+build_docker_image "${tag}" "${image}" "${platform}" "${build_arg}"
