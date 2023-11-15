f0b0f612495d948b77e5bbd093c5c36c6122da4a
Bill Wang <bill.wang>
2023-11-15 13:06:53 +1100

    #1 - [feature] consider to delete the support for linux/arm/v6

--- build.sh
+++ build.sh
@@ -19,7 +19,7 @@ fi
 
 pushd $1
 image="alpine/$1"
-platform="${2:-linux/arm/v7,linux/arm64/v8,linux/arm/v6,linux/amd64,linux/ppc64le,linux/s390x}" 
+platform="${2:-linux/arm/v7,linux/arm64/v8,linux/amd64,linux/ppc64le,linux/s390x}"
 
 curl -H "Cache-Control: no-cache" -sL "https://raw.githubusercontent.com/alpine-docker/multi-arch-libs/stable/functions.sh" -o functions.sh
 source functions.sh
