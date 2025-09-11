44e2ee36b0227b31d1294cf81c37a7ad9e6b2698
Bill <bill@Bills-Mac-mini.local>
2025-09-11 15:42:21 +1000

    add xml

--- xml/version-tag.sh
+++ xml/version-tag.sh
@@ -3,7 +3,7 @@
 curl -H "Cache-Control: no-cache" -sL "https://raw.githubusercontent.com/alpine-docker/multi-arch-docker-images/stable/functions.sh" -o functions.sh
 source functions.sh
 
-image="alpine/mysql"
+image="alpine/xml"
 
 # jq --version
 # jq-1.8.0
